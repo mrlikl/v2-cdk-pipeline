184a28b336a765418f90dcf6cf04cce59ddf265c
S Murali Krishnan <110023377+mrlikl@users.noreply.github.com>
2024-04-04 12:59:19 +0530

    Empty CDK project

---
+++ lib/v2-cdk-pipeline-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class V2CdkPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'V2CdkPipelineQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/v2-cdk-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as V2CdkPipeline from '../lib/v2-cdk-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/v2-cdk-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new V2CdkPipeline.V2CdkPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
